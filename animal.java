5b750d890911ab5e5af0f3f7277379c945988f24
riya4870 <Riya.Sharma@pepsico.com>
2025-08-24 17:25:58 +0000

    prog1

---
+++ animal.java
@@ -0,0 +1,15 @@
+public class animal
+{
+    String name;
+    void setName(String n) {
+        name = n;
+    }
+
+}
+
+    public static void main(String[] args) {
+        // Create an instance of the animal class and set its name
+    animal a = new animal();
+    a.setName("Lion");
+}
+
